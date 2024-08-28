92e4e2ccee581a299a947731262e958f7b093c56
Kimnabin <ducvucong01@gmail.com>
2024-08-28 14:16:45 +0900

    FEAT: add api/users

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/users', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata: [
+            {
+                name: 'congduc',
+                age: 24
+            },
+            {
+                name: 'yenquynh',
+                age: 25
+            },
+            {
+                name: 'conghuy',
+                age: 18
+            }
+        ]
+    })
+})
+
 module.exports = router;
